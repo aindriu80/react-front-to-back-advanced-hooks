a84ccf113533ac6fbf44b65d219b3d72302d68f7
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-01-05 15:57:18 +0000

    useRef Example 1 - Create DOM Reference & Bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css'
+import UseRefExample1 from './components/UseRefExample1'
 
 function App() {
-  return <div className="App">react-front-to-back-advanced-hooks</div>
+  return (
+    <div className="container mt-5">
+      <UseRefExample1 />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/UseRefExample1.jsx
@@ -0,0 +1,39 @@
+import { useRef } from 'react'
+
+function UseRefExample1() {
+  const inputRef = useRef()
+  const paraRef = useRef()
+
+  const onSubmit = (e) => {
+    e.preventDefault(
+      console.log('InputRef.current:', inputRef.current),
+      console.log('inputRef.current.value:', inputRef.current.value),
+      (inputRef.current.value = 'Set inputRef value to Hello'),
+      (inputRef.current.style.backgroundColor = 'purple'),
+      (paraRef.current.innerText = 'Goodbye')
+    )
+  }
+  return (
+    <div>
+      <h1>useRef</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          ref={inputRef}
+          id="name"
+          className="form-control mb-2"
+        />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+        <p onClick={() => inputRef.current.focus()} ref={paraRef}>
+          Hello <b>useRef</b> -> Click on this text to set focus on the input
+          field above
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default UseRefExample1
