1a1eb0278aa4d5f655d479747a83132a999f71dd
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-01-08 10:40:59 +0000

    Custom Hooks 1 - useFetch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 // import UseRefExample2 from './components/UseRefExample2'
 // import UseRefExample3 from './components/UseRefExample3'
 // import UseMemoExample from './components/UseMemoExample'
-import UseCallBackExample from './components/UseCallbackExample'
+// import UseCallBackExample from './components/UseCallbackExample'
+import CustomHookExample1 from './components/CustomHookExample1'
 
 function App() {
   return (
     <div className="container mt-5">
-      <UseCallBackExample />
+      <CustomHookExample1/>
     </div>
   )
 }
